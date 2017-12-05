97150cb88e09dd07529a44ccdebe646e73f2421f
lenconda <1053464288@qq.com>
2017-12-05 11:15:28 +0800

    Finished Login page and now waiting for API to make login requests.

---
+++ src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-container>
+    <el-header></el-header>
+    <el-main>
+      <div class="row-bg el-row is-justify-space-around el-row--flex">
+        <el-col :md="6" :xs="24">
+          <div class="grid-content">
+            <el-card class="box-card">
+              <div slot="header" class="clearfix">
+                <span>请登录</span>
+              </div>
+              <div class="text item">
+                <el-form :model="formLabelAlign">
+                  <el-form-item>
+                    <el-input placeholder="学号/工资号" type="number" v-model="formLabelAlign.name"></el-input>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-input placeholder="密码" type="password" v-model="formLabelAlign.password"></el-input>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button type="primary" @click="submit">登录</el-button>
+                  </el-form-item>
+                </el-form>
+              </div>
+            </el-card>
+          </div>
+        </el-col>
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: "login",
+    mounted() {
+      this.$notify({
+        title: '温馨提示',
+        message: '出于安全考虑，用户名或密码输入错误达到5次将会被冻结账户！',
+        type: 'warning'
+      });
+    },
+    data() {
+      return {
+        formLabelAlign: {
+          name: '',
+          password: ''
+        }
+      };
+    },
+    methods: {
+      submit() {
+        console.log(this.formLabelAlign.name + '\n');
+        console.log(this.formLabelAlign.password + '\n');
+        this.$http.post().then();
+      }
+    }
+  }
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import elementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App';
+import router from './router';
+import vueResource from 'vue-resource';
+
+Vue.use(elementUI);
+Vue.use(vueResource);
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+});
