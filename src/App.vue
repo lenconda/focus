6f1f5164b839155d3e863596350105dad9ee1330
lenconda <1053464288@qq.com>
2017-12-08 11:55:43 +0800

    新增微信浏览器判断

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-    <router-view/>
+    <div v-if="isWeChat()">
+      <router-view/>
+    </div>
+    <div v-else>
+      <div>
+        <h1>请使用微信登录！</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,6 +23,17 @@
           /* Here to place the logic code */
         });
       })
+    },
+    methods: {
+      isWeChat() {
+        var ua = window.navigator.userAgent.toLowerCase();
+        console.log(ua);//mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
+        if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   }
 </script>
