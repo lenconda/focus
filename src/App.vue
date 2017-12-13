6053bc132d69872be9a68b171515fba739e861c9
lenconda <1053464288@qq.com>
2017-12-13 17:44:09 +0800

    新增用户返回时提示，添加二维码

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
     <div v-else>
       <div>
         <h2 class="useWechat">请使用微信访问！</h2>
+        <div class="qrcode">
+          <img src="../static/img/c67934ed63aa99cc30dcd871dcf41518.png" width="100%" alt="">
+          微信扫一扫
+        </div>
       </div>
     </div>
   </div>
@@ -24,10 +28,15 @@
       let _this = this;
       window.addEventListener('focus', function () {
         if (_this.getCookie('counting') == '1') {
-          _this.$http.get('static/api/test.php?action=focus').then(res => {});
-        } else {
-
-        }
+          _this.$http.get('static/api/test.php?action=focus').then(res => {
+            if (res.status == 200) {
+              this.$notify.success({
+                title: '计时继续',
+                message: '继续开始计时了，用心听课哦～'
+              })
+            }
+          });
+        } else {}
       });
       window.addEventListener('blur', function () {
         if (_this.getCookie('counting') == '1') {
@@ -79,5 +88,15 @@
   .useWechat {
     color: #2c3e50;
     margin-top: 2em;
+  }
+  .qrcode {
+    display: block;
+    height: 100px;
+    width: 100px;
+    margin: 0 auto;
+    margin-top: 1em;
+    text-align: center;
+    color: #2c3e50;
+    font-size: 13px;
   }
 </style>
